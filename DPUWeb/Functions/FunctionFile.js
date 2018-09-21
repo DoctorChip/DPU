bca281a50a6d78318dfecde4ff6f709f87054282
Chris Walker <chris@cedita.com>
2018-09-21 15:50:27 +0100

    init

---
+++ DPUWeb/Functions/FunctionFile.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+(function () {
+	Office.initialize = function (reason) {
+
+	};
+})();
+
+
+function toggleProtection(args) {
+	Excel.run(function(context) {
+
+		const sheet = context.workbook.worksheets.getActiveWorksheet();
+		sheet.load('protection/protected');
+
+		return context.sync()
+			.then(
+				function() {
+					if (sheet.protection.protected) {
+						sheet.protection.unprotect('taxi');
+					} else {
+						sheet.protection.protect(null, 'taxi');
+					}
+				}				
+			)
+			.then(context.sync);
+	})
+	.catch(function(error) {
+		console.log("Error: " + error);
+		if (error instanceof OfficeExtension.Error) {
+			console.log("Debug info: " + JSON.stringify(error.debugInfo));
+		}
+	});
+	args.completed();
+}
